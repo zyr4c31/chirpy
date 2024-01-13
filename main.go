3d631e92897d8451279e1ac402e034750e3f4443
zyr4c31 <zyr4c31@gmail.com>
2024-01-13 13:45:11 +0800

    feat: api method working

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -9,13 +10,18 @@ func middlewareCors(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-		w.Header().Set("Cache-Control", "no-cache")
-		if r.Method == "Options" {
+		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
 
 type apiConfig struct {
@@ -24,57 +30,45 @@ type apiConfig struct {
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-cache")
 		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
 	})
 }
 
-func healthz(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
 }
 
 func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits++
-	w.Write([]byte("OK"))
-}
-
-func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits = 0
-	w.Write([]byte("OK"))
+	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileserverHits)))
 }
 
 func main() {
-	port := (":8080")
+	port := "8080"
 
-	api := apiConfig{
+	apicfg := &apiConfig{
 		fileserverHits: 0,
 	}
 
 	mux := http.NewServeMux()
 
-	corsMux := api.middlewareMetricsInc(mux)
+	fs := http.FileServer(http.Dir("."))
 
-	s := &http.Server{
-		Addr:    port,
-		Handler: corsMux,
-	}
+	sp := http.StripPrefix("/app", fs)
 
-	fsh := http.FileServer(http.Dir("."))
-	sph := http.StripPrefix("/app/", api.middlewareMetricsInc(fsh))
-
-	mux.Handle("/app/", api.middlewareMetricsInc(sph))
+	mux.Handle("/app/", apicfg.middlewareMetricsInc(sp))
 
 	mux.HandleFunc("/healthz", healthz)
 
-	mux.HandleFunc("/metrics", api.metrics)
-	mux.HandleFunc("/reset", api.reset)
+	mux.HandleFunc("/reset", apicfg.reset)
+	mux.HandleFunc("/metrics", apicfg.metrics)
 
-	s.ListenAndServe()
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	srv.ListenAndServe()
 }
